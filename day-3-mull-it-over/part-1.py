ecb6b4bf881e3fc3fa0e09c277ccd4d4a2bc6868
rlanderloos <rlanderloos@sbssolutions.be>
2024-12-03 13:20:32 +0100

    Finished Day 3 part 1

---
+++ day-3-mull-it-over/part-1.py
@@ -0,0 +1,32 @@
+﻿import re
+
+mul_pattern = r"(mul\(\d+,\d+\))"
+operator_pattern = r".+(?=\()"
+left_digit_pattern = r"\d+(?=\,)"
+right_digit_pattern = r"\d+(?=\))"
+
+def main():
+    file_object = open("./input.txt", "r")
+
+    lines = file_object.readlines()
+
+    total = 0
+
+    print(lines)
+
+    for line in lines:
+        matches = re.findall(mul_pattern, line)
+        print(matches)
+        for match in matches:
+            operator = re.findall(operator_pattern, match)[0]
+            print(operator)
+            left = re.findall(left_digit_pattern, match)[0]
+            print(left)
+            right = re.findall(right_digit_pattern, match)[0]
+            print(right)
+            total += int(left) * int(right)
+
+    print(f"Total: {total}")
+
+if __name__ == "__main__":
+    main()
